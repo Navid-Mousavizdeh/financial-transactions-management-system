0b1c58d8ab56b04b0110c979a3d13ef6a6c1a2a1
navid-mousavizdeh <72038901+Navid-Mousavizdeh@users.noreply.github.com>
2025-06-09 03:12:17 +0330

    feat: theme store

--- store/index.ts
+++ store/index.ts
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./slices/counterSlice";
+import themeReducer from "./slices/themeSlice";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    theme: themeReducer,
   },
 });
+
+export type RootState = ReturnType<typeof store.getState>;
